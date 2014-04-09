245ab62256ca63e8e1cf95e2b2d7902a0850f970
Jaydeep <jaydeep.w@gmail.com>
2014-04-09 23:33:50 +0530

    show movie list in query when submit

--- movieSearch/platforms/ios/www/js/index.js
+++ movieSearch/platforms/ios/www/js/index.js
@@ -1,3 +1,13 @@
+// You may need to change the API key in case this demo app does not work
+// in future.
+// You can get it here.  http://developer.rottentomatoes.com/
+
+// test key
+var API_KEY = "f8b9qaux45g3ptwezzrrf2hg";
+
+// remove this later
+// var API_KEY = "7ect2zrb24k3fpa2bttzswqn";
+
 var FlickrApp = angular.module('ionicApp', ['ionic', 'ngResource', 'ngRoute'])
 
 .factory('Flickr', function($resource, $q) {
@@ -21,20 +31,17 @@ var FlickrApp = angular.module('ionicApp', ['ionic', 'ngResource', 'ngRoute'])
   }
 })
 
-.controller('Ctrl1', function($scope, Flickr) {
+.controller('Ctrl1', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+    $scope.phoneId = $routeParams.phoneId;
 
-  var doSearch = ionic.debounce(function(query) {
-    Flickr.search(query).then(function(resp) {
-      $scope.photos = resp;
-      // console.log(resp);
-    });
-  }, 500);
-  
-  $scope.search = function() {
-    doSearch($scope.query);
-  }
+    $scope.search = function () {
+      console.log($scope.query);
 
-})
+      if($scope.query != '')
+        searchMovie($scope.query, $scope, $http);
+    }
+  }])
 
 .controller('Ctrl2', ['$scope', '$routeParams',
   function($scope, $routeParams) {
@@ -101,4 +108,36 @@ function MyCtrl($scope, $ionicNavBarDelegate) {
   $scope.goBack = function() {
     $ionicNavBarDelegate.back();
   };
+}
+
+function searchMovie(moviename, $scope, $http) {
+  var APIUrl = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=" + API_KEY + "&q=" + moviename + "&callback=JSON_CALLBACK";
+
+  $http({method: 'JSONP', url: APIUrl})
+  .success(function(data, status, headers, config) {
+
+      var movieResult = data;
+      // this callback will be called asynchronously
+      // when the response is available
+      // console.log('Success ');
+      // console.log(movieResult);
+      var totalResults = movieResult.movies.length;
+      // console.log("total: " + totalResults);
+
+      // if movies are found.
+      // Update the UI.
+      if( totalResults > 0 ) {
+        $scope.movie_results = "Found " + totalResults + " movies";
+        $scope.moviesList = movieResult;
+        console.log(movieResult.movies[0]);
+      } else {
+        $scope.movie_results = "No results found!";
+        $scope.moviesList = [];
+      }
+    })
+  .error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      console.log('Error ' + status);
+    });
 }
