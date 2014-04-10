c3fd19b722978be22921fe64d3328d26007a9181
Jaydeep <jaydeep.w@gmail.com>
2014-04-10 22:46:25 +0530

    include ionicPrompt service

--- movieSearch/platforms/ios/www/js/index.js
+++ movieSearch/platforms/ios/www/js/index.js
@@ -31,8 +31,8 @@ var FlickrApp = angular.module('ionicApp', ['ionic', 'ngResource', 'ngRoute'])
   }
 })
 
-.controller('Ctrl1', ['$scope', '$routeParams', '$http',
-  function($scope, $routeParams, $http) {
+.controller('Ctrl1', ['$scope', '$routeParams', '$http',  '$ionicPopup',
+  function($scope, $routeParams, $http,  $ionicPopup) {
     $scope.phoneId = $routeParams.phoneId;
 
     $scope.search = function () {
@@ -114,30 +114,30 @@ function searchMovie(moviename, $scope, $http) {
   var APIUrl = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=" + API_KEY + "&q=" + moviename + "&callback=JSON_CALLBACK";
 
   $http({method: 'JSONP', url: APIUrl})
-  .success(function(data, status, headers, config) {
+    .success(function(data, status, headers, config) {
 
-      var movieResult = data;
-      // this callback will be called asynchronously
-      // when the response is available
-      // console.log('Success ');
-      // console.log(movieResult);
-      var totalResults = movieResult.movies.length;
-      // console.log("total: " + totalResults);
+        var movieResult = data;
+        // this callback will be called asynchronously
+        // when the response is available
+        // console.log('Success ');
+        // console.log(movieResult);
+        var totalResults = movieResult.movies.length;
+        // console.log("total: " + totalResults);
 
-      // if movies are found.
-      // Update the UI.
-      if( totalResults > 0 ) {
-        $scope.movie_results = "Found " + totalResults + " movies";
-        $scope.moviesList = movieResult;
-        console.log(movieResult.movies[0]);
-      } else {
-        $scope.movie_results = "No results found!";
-        $scope.moviesList = [];
-      }
-    })
-  .error(function(data, status, headers, config) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-      console.log('Error ' + status);
-    });
+        // if movies are found.
+        // Update the UI.
+        if( totalResults > 0 ) {
+          $scope.movie_results = "Found " + totalResults + " movies";
+          $scope.moviesList = movieResult;
+          console.log(movieResult.movies[0]);
+        } else {
+          $scope.movie_results = "No results found!";
+          $scope.moviesList = [];
+        }
+      })
+    .error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.log('Error ' + status);
+      });
 }
